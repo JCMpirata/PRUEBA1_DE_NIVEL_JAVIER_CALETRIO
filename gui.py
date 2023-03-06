8deecb9acc3e2e13517409952d59d934b7333142
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2023-03-06 22:09:04 +0100

    gui a medias

---
+++ gui.py
@@ -0,0 +1,327 @@
+import BDVehiculo as bdv
+import FBicicleta as fbi
+import FCoche as fco
+import FFurgoneta as ffu
+import FQuad as fqu
+import FMotocicleta as fmo
+import helpers as hlp
+from tkinter import *
+from tkinter import ttk
+from tkinter.messagebox import askokcancel, WARNING
+
+
+
+
+class CenterWidgetMixin:
+    def center(self):
+        self.update()
+        w = self.winfo_width()
+        h = self.winfo_height()
+        ws = self.winfo_screenwidth()
+        hs = self.winfo_screenheight()
+        x = int(ws/2 - w/2)
+        y = int(hs/2 - h/2)
+        self.geometry(f"{w}x{h}+{x}+{y}")
+
+
+
+class Ventana_crear_vehiculo(Toplevel, CenterWidgetMixin):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title("Crear vehiculo")
+        self.center()
+        self.config(bg="light green")
+        self.build()
+        self.transient(parent)
+        self.grab_set()
+
+    def build(self):
+        frame = Frame(self)
+        frame.pack(padx = 20, pady = 20)
+
+        Label(frame, text="Tipo de vehiculo").grid(row = 0, column = 0)
+        self.tipo = ttk.Combobox(frame, values=["Bicicleta", "Coche", "Furgoneta", "Quad", "Motocicleta"])
+        self.tipo.grid(row = 0, column = 1)
+        self.tipo.bind("<<ComboboxSelected>>", self.on_tipo_selected)
+
+        self.casillas = {}
+
+        Button(frame, text="Crear", command=self.create).grid(row = 1, column = 0)
+        Button(frame, text="Cancelar", command=self.close).grid(row = 1, column = 1)
+
+    def on_tipo_selected(self, event):
+        tipo = self.tipo.get()
+        if tipo == "Bicicleta":
+            self.casillas = {
+                "Modelo": Entry(self),
+                "Vehiculo": Entry(self),
+                "Marca": Entry(self),
+                "Color": Entry(self),
+                "Ruedas": Entry(self, values = ["2"]),
+                "Precio": Entry(self),
+                "Tipo": ttk.Combobox(self, values=["Montaña", "Ciudad", "Paseo", "BMX"])
+            }
+        elif tipo == "Coche":
+            self.casillas = {
+                "Modelo": Entry(self),
+                "Vehiculo": Entry(self),
+                "Marca": Entry(self),
+                "Color": Entry(self),
+                "Ruedas": Entry(self, values = ["4"]),
+                "Precio": Entry(self),
+                "Velocidad": Entry(self),
+                "Cilindrada": Entry(self)
+            }
+        elif tipo == "Furgoneta":
+            self.casillas = {
+                "Modelo": Entry(self),
+                "Vehiculo": Entry(self),
+                "Marca": Entry(self),
+                "Color": Entry(self),
+                "Ruedas": Entry(self, values = ["4"]),
+                "Precio": Entry(self),
+                "Velocidad": Entry(self),
+                "Cilindrada": Entry(self),
+                "Carga": Entry(self)
+            }
+        elif tipo == "Motocicleta":
+            self.casillas = {
+                "Modelo": Entry(self),
+                "Vehiculo": Entry(self),
+                "Marca": Entry(self),
+                "Color": Entry(self),
+                "Ruedas": Entry(self, values = ["2"]),
+                "Precio": Entry(self),
+                "Tipo": ttk.Combobox(self, values=["Turismo", "Deportiva", "Naked", "Enduro", "Chopper", "Custom"]),
+                "Velocidad": Entry(self),
+                "Cilindrada": Entry(self)
+
+            }
+        elif tipo == "Quad":
+            self.casillas = {
+                "Modelo": Entry(self),
+                "Vehiculo": Entry(self),
+                "Marca": Entry(self),
+                "Color": Entry(self),
+                "Ruedas": Entry(self, values = ["4"]),
+                "Precio": Entry(self),
+                "Velocidad": Entry(self),
+                "Cilindrada": Entry(self),
+                "Tipo": ttk.Combobox(self, values=["Pesado", "Ligero"]),
+                "Carga": Entry(self)
+
+            }
+
+        for i, (key, value) in enumerate(self.casillas.items()):
+            Label(self, text=key).grid(row=i+2, column=0)
+            value.grid(row=i+2, column=1)
+
+    def create(self):
+        tipo = self.tipo.get()
+        if tipo == "Bicicleta":
+            fbi.crear_bicicleta(self.casillas["Modelo"].get(), self.casillas["Vehiculo"].get(), self.casillas["Marca"].get(), self.casillas["Color"].get(), self.casillas["Ruedas"].get(), self.casillas["Precio"].get(), self.casillas["Tipo"].get())
+        elif tipo == "Coche":
+            fco.crear_coche(self.casillas["Modelo"].get(), self.casillas["Vehiculo"].get(), self.casillas["Marca"].get(), self.casillas["Color"].get(), self.casillas["Ruedas"].get(), self.casillas["Precio"].get(), self.casillas["Velocidad"].get(), self.casillas["Cilindrada"].get())
+        elif tipo == "Furgoneta":
+            ffu.crear_furgoneta(self.casillas["Modelo"].get(), self.casillas["Vehiculo"].get(), self.casillas["Marca"].get(), self.casillas["Color"].get(), self.casillas["Ruedas"].get(), self.casillas["Precio"].get(), self.casillas["Velocidad"].get(), self.casillas["Cilindrada"].get(), self.casillas["Carga"].get())
+        elif tipo == "Motocicleta":
+            fmo.crear_motocicleta(self.casillas["Modelo"].get(), self.casillas["Vehiculo"].get(), self.casillas["Marca"].get(), self.casillas["Color"].get(), self.casillas["Ruedas"].get(), self.casillas["Precio"].get(), self.casillas["Tipo"].get(), self.casillas["Velocidad"].get(), self.casillas["Cilindrada"].get())
+        elif tipo == "Quad":
+            fqu.crear_quad(self.casillas["Modelo"].get(), self.casillas["Vehiculo"].get(), self.casillas["Marca"].get(), self.casillas["Color"].get(), self.casillas["Ruedas"].get(), self.casillas["Precio"].get(), self.casillas["Velocidad"].get(), self.casillas["Cilindrada"].get(), self.casillas["Tipo"].get(), self.casillas["Carga"].get())
+
+    def close(self):
+        self.destroy()
+        self.update()
+
+
+class Ventana_editar_vehiculo(Toplevel, CenterWidgetMixin):
+    def __init__(self, parent, vehiculo):
+        Toplevel.__init__(self, parent)
+        self.title("Editar vehiculo")
+        self.center()
+        self.config(bg="light blue")
+        self.build(vehiculo)
+        self.transient(parent)
+        self.grab_set()
+
+    def build(self, vehiculo):
+        frame = Frame(self)
+        frame.pack(padx = 20, pady = 20)
+
+        Label(frame, text="Tipo de vehiculo").grid(row = 0, column = 0)
+        self.tipo = ttk.Combobox(frame, values=["Bicicleta", "Coche", "Furgoneta", "Quad", "Motocicleta"])
+        self.tipo.grid(row = 0, column = 1)
+        self.tipo.bind("<<ComboboxSelected>>", self.on_tipo_selected)
+
+        self.casillas = {}
+
+        Button(frame, text="Editar", command=self.edit).grid(row = 1, column = 0)
+        Button(frame, text="Cancelar", command=self.close).grid(row = 1, column = 1)
+
+    def on_tipo_selected(self, event):
+        tipo = self.tipo.get()
+        if tipo == "Bicicleta":
+            self.casillas = {
+                "Modelo": Entry(self),
+                "Vehiculo": Entry(self),
+                "Marca": Entry(self),
+                "Color": Entry(self),
+                "Ruedas": Entry(self, values = ["2"]),
+                "Precio": Entry(self),
+                "Tipo": ttk.Combobox(self, values=["Montaña", "Ciudad", "Paseo", "BMX"])
+            }
+        elif tipo == "Coche":
+            self.casillas = {
+                "Modelo": Entry(self),
+                "Vehiculo": Entry(self),
+                "Marca": Entry(self),
+                "Color": Entry(self),
+                "Ruedas": Entry(self, values = ["4"]),
+                "Precio": Entry(self),
+                "Velocidad": Entry(self),
+                "Cilindrada": Entry(self)
+            }
+        elif tipo == "Furgoneta":
+            self.casillas = {
+                "Modelo": Entry(self),
+                "Vehiculo": Entry(self),
+                "Marca": Entry(self),
+                "Color": Entry(self),
+                "Ruedas": Entry(self, values = ["4"]),
+                "Precio": Entry(self),
+                "Velocidad": Entry(self),
+                "Cilindrada": Entry(self),
+                "Carga": Entry(self)
+            }
+        elif tipo == "Motocicleta":
+            self.casillas = {
+                "Modelo": Entry(self),
+                "Vehiculo": Entry(self),
+                "Marca": Entry(self),
+                "Color": Entry(self),
+                "Ruedas": Entry(self, values = ["2"]),
+                "Precio": Entry(self),
+                "Tipo": ttk.Combobox(self, values=["Turismo", "Deportiva", "Naked", "Enduro", "Chopper", "Custom"]),
+                "Velocidad": Entry(self),
+                "Cilindrada": Entry(self)
+
+            }
+        elif tipo == "Quad":
+            self.casillas = {
+                "Modelo": Entry(self),
+                "Vehiculo": Entry(self),
+                "Marca": Entry(self),
+                "Color": Entry(self),
+                "Ruedas": Entry(self, values = ["4"]),
+                "Precio": Entry(self),
+                "Velocidad": Entry(self),
+                "Cilindrada": Entry(self),
+                "Tipo": ttk.Combobox(self, values=["Pesado", "Ligero"]),
+                "Carga": Entry(self)
+
+            }
+
+        for i, key in enumerate(self.casillas):
+            Label(self, text=key).grid(row = i + 2, column = 0)
+            self.casillas[key].grid(row = i + 2, column = 1)
+
+        vehiculo = self.master.treeview.focus()
+        values = self.master.treeview.item(vehiculo, "values")
+        self.casillas["Modelo"].insert(0, values[0])
+        self.casillas["Vehiculo"].insert(0, values[1])
+        self.casillas["Marca"].insert(0, values[2])
+        self.casillas["Color"].insert(0, values[3])
+        self.casillas["Ruedas"].insert(0, values[4])
+        self.casillas["Precio"].insert(0, values[5])
+        if tipo == "Bicicleta":
+            self.casillas["Tipo"].insert(0, values[6])
+        elif tipo == "Coche":
+            self.casillas["Velocidad"].insert(0, values[6])
+            self.casillas["Cilindrada"].insert(0, values[7])
+        elif tipo == "Furgoneta":
+            self.casillas["Velocidad"].insert(0, values[6])
+            self.casillas["Cilindrada"].insert(0, values[7])
+            self.casillas["Carga"].insert(0, values[8])
+        elif tipo == "Motocicleta":
+            self.casillas["Tipo"].insert(0, values[6])
+            self.casillas["Velocidad"].insert(0, values[7])
+            self.casillas["Cilindrada"].insert(0, values[8])
+        elif tipo == "Quad":
+            self.casillas["Tipo"].insert(0, values[6])
+            self.casillas["Velocidad"].insert(0, values[7])
+            self.casillas["Cilindrada"].insert(0, values[8])
+            self.casillas["Carga"].insert(0, values[9])
+            
+    def edit(self):
+        tipo = self.tipo.get()
+        if tipo == "Bicicleta":
+            fbi.editar_bicicleta(self.casillas["Modelo"].get(), self.casillas["Vehiculo"].get(), self.casillas["Marca"].get(), self.casillas["Color"].get(), self.casillas["Ruedas"].get(), self.casillas["Precio"].get(), self.casillas["Tipo"].get())
+        elif tipo == "Coche":
+            fco.editar_coche(self.casillas["Modelo"].get(), self.casillas["Vehiculo"].get(), self.casillas["Marca"].get(), self.casillas["Color"].get(), self.casillas["Ruedas"].get(), self.casillas["Precio"].get(), self.casillas["Velocidad"].get(), self.casillas["Cilindrada"].get())
+        elif tipo == "Furgoneta":
+            ffu.editar_furgoneta(self.casillas["Modelo"].get(), self.casillas["Vehiculo"].get(), self.casillas["Marca"].get(), self.casillas["Color"].get(), self.casillas["Ruedas"].get(), self.casillas["Precio"].get(), self.casillas["Velocidad"].get(), self.casillas["Cilindrada"].get(), self.casillas["Carga"].get())
+        elif tipo == "Motocicleta":
+            fmo.editar_motocicleta(self.casillas["Modelo"].get(), self.casillas["Vehiculo"].get(), self.casillas["Marca"].get(), self.casillas["Color"].get(), self.casillas["Ruedas"].get(), self.casillas["Precio"].get(), self.casillas["Tipo"].get(), self.casillas["Velocidad"].get(), self.casillas["Cilindrada"].get())
+        elif tipo == "Quad":
+            fqu.editar_quad(self.casillas["Modelo"].get(), self.casillas["Vehiculo"].get(), self.casillas["Marca"].get(), self.casillas["Color"].get(), self.casillas["Ruedas"].get(), self.casillas["Precio"].get(), self.casillas["Velocidad"].get(), self.casillas["Cilindrada"].get(), self.casillas["Tipo"].get(), self.casillas["Carga"].get())
+
+    def close(self):
+        self.destroy()
+        self.update()
+
+
+class Eliminar(Toplevel):
+    def __init__(self, master, tipo):
+        super().__init__(master)
+        self.master = master
+        self.tipo = tipo
+        self.title("Eliminar")
+        self.geometry("300x200")
+        self.resizable(False, False)
+        self.config(bg = "white")
+        self.grab_set()
+        self.focus_set()
+        self.protocol("WM_DELETE_WINDOW", self.close)
+        self.init_gui()
+
+    def init_gui(self):
+        Label(self, text = "¿Estás seguro de que quieres eliminar este vehículo?", bg = "white").pack(pady = 10)
+        Button(self, text = "Si", command = self.delete).pack(pady = 10)
+        Button(self, text = "No", command = self.close).pack(pady = 10)
+
+    def delete(self):
+        tipo = self.tipo.get()
+        if tipo == "Bicicleta":
+            fbi.eliminar_bicicleta(self.master.treeview.focus())
+        elif tipo == "Coche":
+            fco.eliminar_coche(self.master.treeview.focus())
+        elif tipo == "Furgoneta":
+            ffu.eliminar_furgoneta(self.master.treeview.focus())
+        elif tipo == "Motocicleta":
+            fmo.eliminar_motocicleta(self.master.treeview.focus())
+        elif tipo == "Quad":
+            fqu.eliminar_quad(self.master.treeview.focus())
+        self.destroy()
+        self.update()
+
+    def close(self):
+        self.destroy()
+        self.update()
+
+
+class Ventana_principal(Tk, CenterWidgetMixin):
+    def __init__(self):
+        super().__init__()
+        self.title("Vehículos")
+        self.geometry("800x600")
+        self.resizable(False, False)
+        self.config(bg = "white")
+
+        
+   
+
+            
+
+
+
+            
